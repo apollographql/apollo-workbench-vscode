4aa64baec171f79737f9ba4ac5bae4e04919fb54
Michael Watson <watson@apollographql.com>
2020-12-20 17:30:07 -0800

    Tests can't run in parallel due to code testing limitations of 1 instance at a time

--- src/__tests__/githubCheckTests.ts
+++ src/__tests__/githubCheckTests.ts
@@ -1,14 +1,13 @@
-const cp = require('child_process');
 const fetch = require('node-fetch');
 import { testRunner } from './testRunner';
 
-async function setStatus(context, state, description) {
+async function setStatus(state, description) {
   return fetch(`https://api.github.com/repos/apollographql/apollo-workbench-vscode/statuses/${process.env.GITHUB_SHA}`, {
     method: 'POST',
     body: JSON.stringify({
       state,
       description,
-      context,
+      context: "VSCode Extension Tests",
     }),
     headers: {
       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
@@ -19,39 +18,15 @@ async function setStatus(context, state, description) {
 
 async function githubActionTests() {
   try {
+    await setStatus('pending', 'Running check..'),
 
-    //Set status of PR
-    await Promise.all([
-      await setStatus('No Folder Open Tests', 'pending', 'Running check..'),
-      await setStatus('Folder Open Tests', 'pending', 'Running check..')
-    ]);
+      await testRunner();
+    await testRunner(true);
 
-    //Run tests in parallel
-    await Promise.all([
-      new Promise(async () => {
-        try {
-          await testRunner();
-          await setStatus("No Folder Open Tests", 'success', 'Tests for no workbench folder open passed');
-        } catch (err) {
-          await setStatus("No Folder Open Tests", 'failure', err?.message ? err.message : 'Tests for no workbench folder open failed');
-        }
-      }),
-      new Promise(async () => {
-        try {
-          await testRunner(true);
-          await setStatus("Folder Open Tests", 'success', 'Tests for workbench folder open passed');
-        } catch (err) {
-          await setStatus("Folder Open Tests", 'failure', err?.message ? err.message : 'Tests for workbench folder open failed');
-        }
-      })
-    ]);
+    await setStatus('success', 'Tests for workbench folder open passed');
 
   } catch (err) {
-    const message = err?.message ? err.message : 'Failed to run tests';
-    console.error(message);
-
-    await setStatus("No Folder Open Tests", 'error', message);
-    await setStatus("Folder Open Tests", 'error', message);
+    await setStatus('error', err?.message ? err.message : 'Failed to run tests');
   }
 }
 
