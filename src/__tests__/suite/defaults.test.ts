225e2d81fad23b7cdb9fb0a103803791fb187659
Michael Watson <watson@apollographql.com>
2021-03-18 21:42:37 -0700

    extensions no longer need to be activated before tests

--- src/__tests__/suite/defaults.test.ts
+++ src/__tests__/suite/defaults.test.ts
@@ -6,8 +6,6 @@ import { StateManager } from '../../workbench/stateManager';
 import { NotLoggedInTreeItem } from '../../workbench/studio-graphs/apolloStudioGraphsTreeDataProvider';
 
 suite('Default Workbench Tests', () => {
-    before(activateExtension);
-
     it('Defaults:LocalWorkbenchFiles - Getting Started is displayed', async function () {
         //Setup
         cleanupWorkbenchFiles();
